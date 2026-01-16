91e44f763c883c0ec5518005331185bfa7779b5d
asnamaryallen <asnamaryallen@gmail.com>
2026-01-16 15:06:45 +0530

    all changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
 import { Typography, Button, Box } from "@mui/material";
+import { signInWithPopup } from "firebase/auth";
+import { auth, googleProvider } from "./firebase/firebase";
 
 function App() {
+
+  const handleGoogleLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider);
+      console.log("Logged in user:", result.user.email);
+
+      // later you can redirect to dashboard
+      // navigate("/dashboard");
+
+    } catch (error) {
+      console.error("Login error:", error.message);
+    }
+  };
+
   return (
     <Box
       sx={{
@@ -14,37 +30,34 @@ function App() {
       }}
     >
       <Typography variant="h4">
-        BillVault 
+        BillVault
       </Typography>
 
       <Typography variant="body1">
         College Bill Management System
       </Typography>
 
-     <Button
-  variant="contained"
-  size="large"
-  sx={{
-    borderRadius: "50px",
-    paddingX: 4,
-    paddingY: 1.5,
-    textTransform: "none",
-    fontSize: "16px",
-    backgroundColor: "#00bfa5",
-    boxShadow: "0px 6px 16px rgba(0,191,165,0.4)",
-    "&:hover": {
-      backgroundColor: "#00a896",
-    }
-  }}
->
-  Login with Google
-</Button>
-
-
-
+      <Button
+        variant="contained"
+        size="large"
+        onClick={handleGoogleLogin}   // 🔥 IMPORTANT
+        sx={{
+          borderRadius: "50px",
+          paddingX: 4,
+          paddingY: 1.5,
+          textTransform: "none",
+          fontSize: "16px",
+          backgroundColor: "#00bfa5",
+          boxShadow: "0px 6px 16px rgba(0,191,165,0.4)",
+          "&:hover": {
+            backgroundColor: "#00a896",
+          }
+        }}
+      >
+        Login with Google
+      </Button>
     </Box>
   );
 }
 
 export default App;
-
